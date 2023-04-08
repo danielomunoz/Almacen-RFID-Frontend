630731a0b141f18b8946453ae35415360f6da369
danielomunoz <dma@dma.com>
2023-04-08 12:01:05 +0200

    Maqueta modales del navbar

--- src/componentes/compartidos/Navbar.jsx
+++ src/componentes/compartidos/Navbar.jsx
@@ -21,7 +21,7 @@ function Navbar() {
                         <a className="nav-link" href="#">Objetos</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Mi Perfil</a>
+                        <a className="nav-link" data-bs-toggle="modal" data-bs-target="#miPerfilModal">Mi Perfil</a>
                     </li>
                     <li className="nav-item dropdown">
                         <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -60,7 +60,68 @@ function Navbar() {
                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
-                ...
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Nombre</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Familia</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Categoría</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Subcategoría</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Número de serie</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Estado en almacén</label>
+                    <select className="form-select" aria-label="Default select example">
+                        <option value="0">Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Registrado desde</label>
+                    <input type="date" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Registrado hasta</label>
+                    <input type="date" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Localización</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Código RFID</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Última acción desde</label>
+                    <input type="date" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Última acción hasta</label>
+                    <input type="date" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Estado del objeto</label>
+                    <select className="form-select" aria-label="Default select example">
+                        <option value="0">Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
             </div>
             <div className="modal-footer">
                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
@@ -77,7 +138,60 @@ function Navbar() {
                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
-                ...
+                <div className='avatar mb-3'>
+                    <img src="..." alt="Mi imagen" className="img-thumbnail" />
+                    <button type="button" className="btn btn-outline-warning">Cambiar imagen</button>
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Nombre</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Familia</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Categoría</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Subcategoría</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Número de serie</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Estado en almacén</label>
+                    <select className="form-select" aria-label="Default select example">
+                        <option value="0">Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Código RFID del Propietario del objeto</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Localización</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Código RFID</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Estado del nuevo objeto</label>
+                    <select className="form-select" aria-label="Default select example">
+                        <option value="0">Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
             </div>
             <div className="modal-footer">
                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
@@ -96,6 +210,46 @@ function Navbar() {
             <div className="modal-footer">
                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
                 <button type="button" className="btn btn-danger">Sí, cerrar sesión</button>
+            </div>
+            </div>
+        </div>
+      </div>
+      <div className="modal fade" id="miPerfilModal" tabIndex="-1" aria-labelledby="miPerfilModalLabel" aria-hidden="true">
+        <div className="modal-dialog modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div className="modal-content">
+            <div className="modal-header">
+                <h5 className="modal-title" id="miPerfilModalLabel">Mi Perfil</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+                <div className='avatar mb-3'>
+                    <img src="..." alt="Mi imagen" className="img-thumbnail" />
+                    <button type="button" className="btn btn-outline-warning">Cambiar imagen</button>
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Nombre</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Móvil</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">DNI</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Código RFID</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" disabled />
+                </div>
+            </div>
+            <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                <button type="button" className="btn btn-warning">Actualizar perfil</button>
             </div>
             </div>
         </div>
