da320451fcda317f01cabfd20ab8bd3c941b53ff
danielomunoz <dma@dma.com>
2023-04-10 11:06:17 +0200

    Crea router de la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { Route, Routes, Navigate, useLocation } from "react-router-dom"
 import './App.css'
 
 import PrincipalPagina from './componentes/paginas/PrincipalPagina';
+import LoginPagina from './componentes/paginas/LoginPagina';
 
 function App() {
+
+  const [estoyAutenticado, setEstoyAutenticado] = useState(true);
+  const location = useLocation();
+  const query = useLocation().search;
+  console.log(location.pathname);
+  console.log(query);
   
   return (
     <>
-      <PrincipalPagina />
+      {
+        estoyAutenticado
+          ?
+            <Routes>
+              <Route path="/" element={<PrincipalPagina autenticado={setEstoyAutenticado} />} />
+              <Route path="/rastreo" element={<PrincipalPagina autenticado={setEstoyAutenticado} />} />
+              <Route path="/objetos" element={<PrincipalPagina autenticado={setEstoyAutenticado} />} />
+              <Route path="/mis-objetos" element={<PrincipalPagina autenticado={setEstoyAutenticado} />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          :
+            <Routes>
+              <Route path="/login" element={<LoginPagina autenticado={setEstoyAutenticado} />} />
+              <Route path="*" element={<Navigate to="/login" />} />
+            </Routes>
+      }
     </>
   )
 }
---
+++ src/componentes/paginas/LoginPagina.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './LoginPagina.css'
+
+
+function LoginPagina() {
+  
+  return (
+    <>
+      <p>Login works!</p>
+    </>
+  )
+}
+
+export default LoginPagina
